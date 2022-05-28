1cf0da87c5ec303dc482cf359f718e58ee1ead24
x64 <alberto_arias_x64@gmail.com>
2022-05-27 20:58:33 -0500

    beta ready

--- main.js
+++ main.js
@@ -4,20 +4,32 @@ function redir(link) {
 
 function read_json() {
     const target = document.querySelector('.bg_img')
+
+    const loop = (res) => {
+        const list = document.querySelectorAll('.img_bg')
+        let index = 0
+        setInterval(() => {
+            if (index === 0){
+                list[list.length-1].classList.add('hide')
+                list[list.length-1].classList.remove('show')
+            }
+            list[index].classList.add('hide')
+            list[index].classList.remove('show')
+            index += 1
+            list[index].classList.remove('hide')
+            list[index].classList.add('show')
+            //console.log(index)
+            if (index === res.length) index = 0
+        }, 4000);
+    }
+
     fetch('./imgs.json')
         .then(res => res.json())
         .then(res => {
-            let index = 0
-            res.forEach((element) => {
+            target.innerHTML += `<img class="img_bg show" src="./images/${res[res.length-1]}" alt="">`
+            res.forEach((element, index) => {
                 target.innerHTML += `<img class="img_bg hide" src="./images/${element}" alt="">`
+                if (index === res.length -1) loop(res)
             })
-            const list = document.querySelectorAll('.img_bg')
-            setInterval(() => {
-                list[index].classList.add('hide')
-                list[index+1].classList.remove('hide')
-                list[index+1].classList.add('show')
-                index += 1
-                if (index === res.length-1) index = 0
-            }, 2000);
         })
 }
